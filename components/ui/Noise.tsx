6f67fd8b787a7184139929e65a95529fcd847d6c
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-09-06 21:21:45 +0530

    gallary ui bg

---
+++ components/ui/Noise.tsx
@@ -0,0 +1,13 @@
+const Noise = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 700 700" width="700" height="700"><defs><filter id="nnnoise-filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="linearRGB">
+            <feTurbulence type="fractalNoise" baseFrequency="0.2" numOctaves="4" seed="15" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+            <feSpecularLighting surfaceScale="7" specularConstant="2.8" specularExponent="20" lighting-color="#ffffff" x="0%" y="0%" width="100%" height="100%" in="turbulence" result="specularLighting">
+                <feDistantLight azimuth="3" elevation="64"></feDistantLight>
+            </feSpecularLighting>
+
+        </filter></defs><rect width="700" height="700" fill="transparent"></rect><rect width="700" height="700" fill="#ffffff" filter="url(#nnnoise-filter)"></rect></svg>
+    )
+}
+
+export default Noise
